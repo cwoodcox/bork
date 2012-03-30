3f0e6acfd583802cfe449bd9036f5a1a60487268
Corey Woodcox <corey.woodcox@gmail.com>
2012-03-30 17:52:18 -0600

    everything hooks up as expected and it's shiny and clean

--- lib/bork/cli.rb
+++ lib/bork/cli.rb
@@ -1,5 +1,14 @@
+require 'thor'
+
 module Bork
   class CLI < Thor
+    include Thor::Actions
     check_unknown_options!
+
+    desc "init", "Create a bork configuration in the current project/directory"
+    def init
+      run "mkdir -p config"
+      run "touch config/bork.rb"
+    end
   end
 end
